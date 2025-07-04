8cd99fd589bf76e1e57822038ba906bc32c11b5e
amegha-10 <moonlightshadow943@gmail.com>
2025-07-04 21:13:56 +0530

    tips.py

--- tips.py
+++ tips.py
@@ -2,35 +2,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the dataset
 df = pd.read_csv("tips.csv")
+Fig, axs = plt.subplots(2, 2, figsize=(12, 10))
 
-# Create 2x2 subplot layout
-fig, axs = plt.subplots(2, 2, figsize=(12, 10))
 
-# 1. Histogram of total bill
 axs[0, 0].hist(df['total_bill'], bins=20, color='skyblue', edgecolor='black')
 axs[0, 0].set_title('Histogram of Total Bill')
 axs[0, 0].set_xlabel('Total Bill')
 axs[0, 0].set_ylabel('Frequency')
 
-# 2. Boxplot of tip amounts
+
 axs[0, 1].boxplot(df['tip'], vert=True)
 axs[0, 1].set_title('Boxplot of Tips')
 axs[0, 1].set_ylabel('Tip Amount')
 
-# 3. Pie chart of gender distribution
+
 gender_counts = df['sex'].value_counts()
 axs[1, 0].pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', colors=['lightcoral', 'lightskyblue'])
 axs[1, 0].set_title('Gender Distribution')
 
-# 4. Bar chart of average total bill by day
+
 avg_total_bill = df.groupby('day')['total_bill'].mean()
 axs[1, 1].bar(avg_total_bill.index, avg_total_bill.values, color='lightgreen')
 axs[1, 1].set_title('Average Total Bill by Day')
 axs[1, 1].set_xlabel('Day')
 axs[1, 1].set_ylabel('Avg Total Bill')
 
-# Adjust layout
 plt.tight_layout()
 plt.show()
